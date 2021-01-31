aaf15d88a39c5aed04981dc8c5909690362988a0
s2043106 <qbz147@126.com>
2021-01-31 00:50:15 +0000

    q4

---
+++ code/create_figure.py
@@ -0,0 +1,46 @@
+import matplotlib.pyplot as plt
+import re
+import numpy as np
+
+
+def q2_loss():
+    data = open('data_q2.txt')
+    x = []
+    y = []
+    for line in data:
+        y.append(round(float(re.split(' ', line)[-1][:-1]), 2))
+        x.append(re.split(' ', line)[1][:-1])
+    print(x, y)
+    plt.plot(x, y, label='q2_loss')
+    plt.legend()
+    plt.savefig('./q2')
+
+
+def q3_loss():
+    data = open('data_q3.txt')
+    x = []
+    y = []
+    for line in data:
+        # print(re.split('  ', line))
+        y.append(round(float(re.split('  ', line)[5][10:]), 2))
+        x.append(re.split(' ', line)[1][:-1])
+    plt.plot(x, y, label='q3_loss')
+    plt.xticks(range(0, 51, 5))
+    plt.legend()
+    plt.savefig('./q3')
+
+def q3_acc():
+    data = open('data_q3.txt')
+    x = []
+    y = []
+    for line in data:
+        # print(re.split(' ', line))
+        y.append(round(float(re.split(' ', line)[-1][:-1]),2))
+        x.append(re.split(' ', line)[1][:-1])
+    plt.plot(x,y,label = 'q3_loss')
+    plt.xticks(range(0,51,5))
+    plt.legend()
+    plt.savefig('./q3_acc')
+
+
+q3_loss()
